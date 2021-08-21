341254ac864a81630a1dc74f62256e4f271393e0
Hellen02Henandez2021 <87906169+Hellen02Henandez2021@users.noreply.github.com>
2021-08-21 17:08:26 -0600

    primer commit

---
+++ Division.py
@@ -0,0 +1,8 @@
+print("Division de numeros enteros")
+
+Nunm1="trunc(Nunm1) and Nunm2=trunc(Num2)" 
+
+if "Si un numero positivo"
+print("Se podra realizar la division")
+"else":
+("No se podra realizar la divison")
